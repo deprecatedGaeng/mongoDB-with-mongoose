c39ad9ff70d1f0c5edeecdfa736c7297f1400549
gaeng <love@nextm.kr>
2019-06-24 17:18:01 +0900

    modify

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/signup',signupRoute);
 app.use('/login',loginRoute);
 //MONGOOSE
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI/* { useMongoClient: true }*/)
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true })
   .then(() => console.log('Successfully connected to mongodb'))
   .catch(e => console.error(e));
 
