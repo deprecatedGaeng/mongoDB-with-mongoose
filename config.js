b48df3b4b106bac42cb56f8fb1c9f58f19941d3c
gaeng <love@nextm.kr>
2019-06-24 16:35:04 +0900

    add module

---
+++ config.js
@@ -0,0 +1,9 @@
+export const confJwt = {
+    expiresIn : 1000 * 60 * 60 * 24 * 1, // A day 1000 * 60 * 60 * 24 * 1
+    secret : 'gaengsworld-jwt-secret'
+}
+
+export const confCrypto =  {
+    secret : 'gaengsworld',
+    pattern : 'aes-256-cbc'
+}
